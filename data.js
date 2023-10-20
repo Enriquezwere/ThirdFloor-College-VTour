60d95ad070933516745188091e916a2fced61390
Enriquezwere <enriquez.marieleanne@gmail.com>
2023-10-20 11:43:55 -0800

    Third Floor (College) VTour

---
+++ data.js
@@ -0,0 +1,290 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3rd-floor",
+      "name": "3rd Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -2.813659821584423,
+        "pitch": 0.12275736719932695,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9121768290467056,
+          "pitch": 0.1046101791902565,
+          "rotation": 0,
+          "target": "1-guidance-office-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0545647713519237,
+          "pitch": 0.28678428145606993,
+          "title": "3rd Floor (College Campus)",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-guidance-office-",
+      "name": "Guidance office ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.04808638276798405,
+        "pitch": -0.00743607527499357,
+        "fov": 1.2900243729711385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.059088405446875,
+          "pitch": 0.2716123499288514,
+          "rotation": 0,
+          "target": "2-hallway-1"
+        },
+        {
+          "yaw": -1.0392061437655435,
+          "pitch": 0.2126717372014859,
+          "rotation": 0,
+          "target": "0-3rd-floor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.009603586392682217,
+          "pitch": 0.03377886669476027,
+          "title": "Guidance Office",
+          "text": "Guidance office is a school facility staffed by guidance counselors who offer students academic and emotional support, assist with college and career planning, provide advice on course selection, and address personal or social issues that may affect a student's well-being and educational progress.<br><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-hallway-1",
+      "name": "Hallway 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 2.9557531122325553,
+        "pitch": 0.0701968914739215,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.010895875331828009,
+          "pitch": 0.21624600443158926,
+          "rotation": 0,
+          "target": "1-guidance-office-"
+        },
+        {
+          "yaw": 3.0092347623388456,
+          "pitch": 0.1730362802639629,
+          "rotation": 0,
+          "target": "3-hallway-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hallway-2",
+      "name": "Hallway 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.09973355279768548,
+        "pitch": 0.019829534066648336,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9367803690155743,
+          "pitch": 0.2277345251408267,
+          "rotation": 0,
+          "target": "5-comfort-room"
+        },
+        {
+          "yaw": -0.035386298391259174,
+          "pitch": 0.17313897371860776,
+          "rotation": 0,
+          "target": "4-hallway-3"
+        },
+        {
+          "yaw": 3.0408568495856887,
+          "pitch": 0.1491185228995171,
+          "rotation": 0,
+          "target": "2-hallway-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hallway-3",
+      "name": "Hallway 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1230112646509163,
+          "pitch": 0.18546131048625902,
+          "rotation": 0,
+          "target": "3-hallway-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-comfort-room",
+      "name": "Comfort Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.928487065888918,
+          "pitch": 0.15993868090279761,
+          "rotation": 0,
+          "target": "4-hallway-3"
+        },
+        {
+          "yaw": 2.849208957333346,
+          "pitch": 0.19197988684609868,
+          "rotation": 0,
+          "target": "3-hallway-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.03239097410349068,
+          "pitch": 0.11432008975291552,
+          "title": "Comfort Room",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
